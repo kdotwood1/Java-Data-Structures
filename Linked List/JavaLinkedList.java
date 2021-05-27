995bcf17d863891a1eda1b380f342dd63141dc46
kdotwood1 <81205156+kdotwood1@users.noreply.github.com>
2021-05-27 23:03:29 +1200

    shortening and checking methods

--- Linked List/JavaLinkedList.java
+++ Linked List/JavaLinkedList.java
@@ -16,19 +16,16 @@ class JavaLinkedList
   }
 
 /* method that removes strings from a linked list */
-  public void remove(String str){
-    Node temp = head;                           // starts at the top of the list
-    if(temp.key.equals(str)){                   // checks the first Node if the string matches
-    temp.next = temp.next.next;                 // point the current node to two nodes ahead and let garbage collection remove the node
-    }
-    while(temp.next != null){                   // opens a loop to iterate through the rest of the list
-      if (temp.next.key.equals(str)){           // remove string from list by making the next of 
-        temp.next = temp.next.next;             // Node before equal the reference to the Node after   
-        return;                                 
-      } else { temp = temp.next; }
-    }
-    return;
-  }
+   public void remove(String str){
+      Node temp = head;                                   // starts at the top of the list
+      while(temp.next != null){        // opens a loop to iterate through the rest of the list
+         if(temp.key.equals(str)) {
+            temp.next = temp.next.next;
+            return
+         } else { temp = temp.next; }
+      }
+      return;
+   }
   
 /* a method that returns true if the linked list 
 contains a node with a string, false otherwise */
@@ -38,8 +35,8 @@ contains a node with a string, false otherwise */
       if(temp.key.equals(str)){                 //.equals() is a method to compare strings
         return true;                            // if the string is found, return true
       } else { temp = temp.next; }              // else traverse to the next node in the linked list
-      return false;                             // if not found return false
     }
+    return false;                               // if not found return false
   }
 
 /* method that counts the number of items in the 
